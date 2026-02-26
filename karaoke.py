534b192cffa3961749c8230f225965413759f750
laiyu <ashenzing@gmail.com>
2026-02-26 14:26:23 -0600

    add download pct

--- karaoke.py
+++ karaoke.py
@@ -401,9 +401,8 @@ class Karaoke:
 
 		ydl_opts = {
 			'outtmpl': self.download_path+'tmp/'+dl_path,
-		    'progress_hooks': [progress_hook(song_url)],
-		    'cookiefile': os.path.join(self.base_path, 'cookies.txt'),
-		    'force_generic_extractor': True
+		    # 'progress_hooks': [progress_hook(song_url)],
+		    'cookiefile': os.path.join(self.base_path, 'cookies.txt')
 		}
 
 		with YoutubeDL(ydl_opts) as ydl:

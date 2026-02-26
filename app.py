8cdb470d04bfa8fcf779840226335c1958f1fa64
laiyu <ashenzing@gmail.com>
2026-02-26 17:55:23 -0600

    add download pct

--- app.py
+++ app.py
@@ -565,7 +565,7 @@ def check_download():
 @app.route("/check_download_pct", methods = ["POST"])
 def check_download_pct():
 	pct = K.downloading_songs_pct.get(request.values.get('url', None), 1)
-	return str(pct)
+	return pct
 
 @app.route("/qrcode")
 def qrcode():

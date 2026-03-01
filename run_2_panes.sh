48c963d75fb5d8db843993ca977e790bdc496b59
laiyu <ashenzing@gmail.com>
2026-02-28 20:57:22 -0600

    update

--- run_2_panes.sh
+++ run_2_panes.sh
@@ -8,7 +8,7 @@ tmux new-session -d -s $SESSION_NAME
 
 # Split the initial window horizontally (or use -v for vertical)
 # The -t flag targets the specific session/window (0) and pane (0)
-tmux split-window -h -t ${SESSION_NAME}:0.0 -l 15
+tmux split-window -h -t ${SESSION_NAME}:0.0 -l 10
 
 # Send the command to the first pane (pane 0) and press Enter
 tmux send-keys -t ${SESSION_NAME}:0.0 "$APP1_CMD" ENTER
